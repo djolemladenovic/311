fdd154e9e83a1681b9e569ab54e493d812e6e896
Djordje Mladenović <127609209+djolemladenovic@users.noreply.github.com>
2024-03-22 12:27:52 +0100

    Update program.c
    bla bla

--- program.c
+++ program.c
@@ -3,5 +3,7 @@ int main()
 {
 https://github.com/djolemladenovic/311.git
   printf("Dobar dan , kako ste\n);
+      printf("Dobar dan , kako ste\n);
+      printf("Dobar dan , kako ste\n);
 return 0;
 }
